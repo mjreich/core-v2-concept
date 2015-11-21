b0d44cdfc73d5d372f869b8c3219790866a0d099
Mike Reich <mjreich@gmail.com>
2015-11-20 17:30:01 -0800

    Fixed dispatcher

--- src/Dispatcher.js
+++ src/Dispatcher.js
@@ -2,7 +2,7 @@
 * @Author: mike
 * @Date:   2015-11-17 13:22:24
 * @Last Modified 2015-11-20
-* @Last Modified time: 2015-11-20 17:17:07
+* @Last Modified time: 2015-11-20 17:29:31
 */
 
 'use strict';
@@ -24,33 +24,45 @@ export default class Dispatcher extends EventEmitter {
   emit(event) {
     var cb = (...args) => {
       args.unshift(event);
-    try {
-      if (arguments[0] == "newListener") return super.emit.apply(this, arguments)
-
-      var args = Array.prototype.slice.call(arguments)
-      var beforeArgs = Array.prototype.slice.call(arguments)
-      beforeArgs[0] = beforeArgs[0]+".before"
-      var afterArgs = Array.prototype.slice.call(arguments)
-      afterArgs[0] = afterArgs[0]+".after"
-
-      super.once.apply(this, [beforeArgs[0], ((a) => {
-        return () => {super.emit.apply(this, a)}
-      })(args)])
-
-      super.once.apply(this, [arguments[0], ((a) => {
-        return () => {
-          this._registered.forEach((cb) => {
-            cb(arguments);
-          });
-          super.emit.apply(this, a)
-        }
-      })(afterArgs)])
-      
-      super.emit.apply(this, beforeArgs)
-    } catch (e) {
-      var logger = console
-      if (this.log && this.log.error) logger = this.log
-      logger.error("Exception processing event: " + arguments[0], e.stack, e)
+      super.emit.apply(this, args);
+    }
+    return {
+      with: cb
     }
   }
+
+  // emit(event) {
+  //   var cb = (...args) => {
+  //     args.unshift(event);
+  //     try {
+  //       if (arguments[0] == "newListener") return super.emit.apply(this, arguments)
+
+  //       var args = Array.prototype.slice.call(arguments)
+  //       var beforeArgs = Array.prototype.slice.call(arguments)
+  //       beforeArgs[0] = beforeArgs[0]+".before"
+  //       var afterArgs = Array.prototype.slice.call(arguments)
+  //       afterArgs[0] = afterArgs[0]+".after"
+
+  //       super.once.apply(this, [beforeArgs[0], ((a) => {
+  //         return () => {super.emit.apply(this, a)}
+  //       })(args)])
+
+  //       super.once.apply(this, [arguments[0], ((a) => {
+  //         return () => {
+  //           this._registered.forEach((cb) => {
+  //             cb(arguments);
+  //           });
+  //           super.emit.apply(this, a)
+  //         }
+  //       })(afterArgs)])
+        
+  //       super.emit.apply(this, beforeArgs)
+  //     } catch (e) {
+  //       var logger = console
+  //       if (this.log && this.log.error) logger = this.log
+  //       logger.error("Exception processing event: " + arguments[0], e.stack, e)
+  //     }
+  //   }
+  // }
+
 } 
